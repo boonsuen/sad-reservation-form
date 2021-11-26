3f03a167b2aa9736ff1666bfcd311f1c9fb56112
Boonsuen Oh <boonsuenoh@gmail.com>
2021-11-26 22:32:17 +0800

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import Head from 'next/head';
-import { Button, Space, Typography, Input, Divider } from 'antd';
+import { Button, Space, Typography, Input, Divider, Select } from 'antd';
 import styled from 'styled-components';
 import {
   CalendarOutlined,
-  GithubOutlined,
   SearchOutlined,
   UserOutlined,
+  SecurityScanOutlined,
 } from '@ant-design/icons';
 
 const { Title, Paragraph } = Typography;
+const { Option } = Select;
 
 const Main = styled.main`
   display: flex;
@@ -16,15 +17,24 @@ const Main = styled.main`
   justify-content: center;
   align-items: center;
   background-color: #fef1ef;
+  padding-bottom: 50px;
 
   .logo {
     width: 270px;
+  }
+  .verificationContainer {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    .ant-input-prefix {
+      margin-right: 10px;
+    }
   }
 `;
 
 const Content = styled.div`
   background-color: #fff;
-  padding: 30px;
+  padding: 30px;  
   border-radius: 10px;
   box-shadow: 13px 37px 84px 0px rgba(50, 55, 72, 0.1);
 `;
@@ -75,8 +85,12 @@ const MemberInfoContainer = styled.div`
 
 const SelectBookInputs = styled.div`
   display: grid;
-  grid-template-columns: 360px 1fr 1fr;
-  gap: 30px;
+  grid-template-columns: 370px 1fr 1fr;
+  gap: 32px;
+
+  .ant-input-prefix {
+    margin-right: 10px;
+  }
 `;
 
 const HomePage: React.FC = () => {
@@ -148,35 +162,73 @@ const HomePage: React.FC = () => {
               <Label>Search by Book Title</Label>
               <Input
                 size="large"
-                placeholder="e.g. 21GP1234"
+                placeholder="Enter book title"
                 style={{ fontSize: 16 }}
                 prefix={<SearchOutlined />}
               />
             </div>
             <div>
               <Label>Filter by Category</Label>
-              <Input
+              <Select
+                showSearch
+                placeholder="Select a category"
+                optionFilterProp="children"
+                filterOption={(input, option) =>
+                  option?.children.toLowerCase().indexOf(input.toLowerCase()) >=
+                  0
+                }
+                style={{ width: '100%' }}
                 size="large"
-                placeholder="e.g. 21GP1234"
-                style={{ fontSize: 16 }}
-                prefix={<SearchOutlined />}
-              />
+              >
+                <Option value="computer-science">Computer Science</Option>
+                <Option value="encyclopedia">Encyclopedia</Option>
+                <Option value="horror">Horror</Option>
+              </Select>
             </div>
             <div>
               <Label>Filter by Author</Label>
-              <Input
+              <Select
+                showSearch
+                placeholder="Select a author"
+                optionFilterProp="children"
+                filterOption={(input, option) =>
+                  option?.children.toLowerCase().indexOf(input.toLowerCase()) >=
+                  0
+                }
+                style={{ width: '100%' }}
                 size="large"
-                placeholder="e.g. 21GP1234"
-                style={{ fontSize: 16 }}
-                prefix={<SearchOutlined />}
-              />
+              >
+                <Option value="alison-chong">Alison Chong</Option>
+                <Option value="alan-turing">Alan Turing</Option>
+                <Option value="andrew-garfield">Andrew Garfield</Option>
+              </Select>
             </div>
           </SelectBookInputs>
+          <Divider />
+          <div>
+            <Label>
+              Verification code <span className="required">*</span>
+            </Label>
+            <div className="verificationContainer">
+              <Input
+                size="large"
+                placeholder="e.g. 123456"
+                style={{ width: '230px', fontSize: 16 }}
+                prefix={<SecurityScanOutlined />}
+              />
+              <Button type="primary" size="large">
+                Get Code
+              </Button>
+              <Button type="primary" size="large" disabled>
+                Verify
+              </Button>
+            </div>
+          </div>
+          <Divider />
           <Button type="primary" size="large">
             Reserve
           </Button>
         </Content>
-        <Paragraph>Welcome back</Paragraph>
       </Main>
     </>
   );
